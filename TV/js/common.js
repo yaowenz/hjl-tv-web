0726ea5eaf9fb866c88bb5149eb60c822aed249c
olive <353131202@qq.com>
2016-06-09 21:52:26 +0800

    video/js

--- TV/js/common.js
+++ TV/js/common.js
@@ -1,7 +1,15 @@
 $(function(){
+	var conH = $(".right").height();
+	if(conH>1080){
+		$(".scroll_bar").show();
+	}
+	
 	$(".icons>div").height( $(".icons").width()-10 );	
 	$(".menu").click(function(){$(".list").animate({"right":"0px"},300)});	
-	$(".list li").click(function(){$(this).siblings().removeClass("current");$(this).addClass("current");})	
+	$(".list li a").click(function(){
+		$(this).parent().siblings().children().removeClass("current");
+		$(this).addClass("current");
+	})
 	var step = 0;
 	var items = $(".txt .box").length;	
 	$(".js-next").click(function(){
@@ -41,6 +49,22 @@ $(function(){
 		$(".js-prev").attr("src","images/prev2.png");
 	})
 	
+	
+	$(".scroll_bar .down").hover(function(){
+		$(".scroll_bar .down").attr("src","images/down2.png");
+	},function(){
+		$(".scroll_bar .down").attr("src","images/down.png");
+	})
+	$(".scroll_bar .up").hover(function(){
+		$(".scroll_bar .up").attr("src","images/up2.png");
+	},function(){
+		$(".scroll_bar .up").attr("src","images/up.png");
+	})
+	
+	
+	$(".list .close").click(function(){
+		$(".list").animate({"right":"-260px"},300)
+	})
 	
 /*循环播放
  * var timer = setInterval(start,3000)
