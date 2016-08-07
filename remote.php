ccf2c9e3efaf2543f8b7232d5bd126e5ce10d903
Yaowen Zhang <yaowen.zhang@live.com>
2016-08-07 11:38:58 +0800

    update sign

--- remote.php
+++ remote.php
@@ -1,7 +1,18 @@
 <?php
+if(file_exists('config.php')) {
+    include('config.php');
+}
+
 class RemoteService {
 
-    public static function httpGet($url) {
+    public static function httpGet($url, $params) {
+        
+        $params = self::filterParams($params);
+        $params['sign'] = self::calcSignature($params);
+        
+        $url .= '?';
+        $url .= http_build_query($params);
+        
         $ch = curl_init($url);
         
         //curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -32,18 +43,64 @@ class RemoteService {
     }
 
     public static function getJobs() {
-        $params = http_build_query(['identifier' => $_GET['identifier'], 'page' => $_GET['page']]);
-        $url = 'http://api.haojialian.net/shop/tv/jobs?' . $params; // TODO config
+        $params = ['identifier' => $_GET['identifier'], 'page' => $_GET['page']];
+        $url = 'http://api.haojialian.net/shop/tv/jobs';
         $data = self::httpGet($url, $params);
         return $data;
     }
     
     public static function getShopInfo() {
-        $params = http_build_query(['identifier' => $_GET['identifier']]);
-        $url = 'http://api.haojialian.net/shop/tv/info?' . $params; // TODO config
+        $params = ['identifier' => $_GET['identifier']];
+        $url = 'http://api.haojialian.net/shop/tv/info';
         $data = self::httpGet($url, $params);
         return $data;
     }
+    
+    protected static function filterParams($params)
+    {
+        foreach ($params as $key => $value) {
+            if (is_null($value)) {
+                $params[$key] = '';
+            }
+        }
+        
+        $phrase = '';
+        for($i = 0; $i < 8; $i++) {
+            $phrase .= mt_rand(0, 9);
+        }
+        
+        $params['nonce'] = $phrase;
+        $params['ts'] = time();
+    
+        return $params;
+    }
+    
+    protected static function calcSignature($params, $data = null)
+    {
+        $key = defined('PLATFORM_API_KEY') ? PLATFORM_API_KEY : '';
+        $str = self::paramsToSignatureString($params);
+        $str .= 'key='.$key;
+        if (is_string($data)) {
+            $str .= $data;
+        }
+    
+        return md5($str);
+    }
+    
+    protected static function paramsToSignatureString($params)
+    {
+        $str = '';
+    
+        ksort($params, SORT_STRING);
+        foreach ($params as $key => $value) {
+            if (!is_array($value)) {
+                $str .= $key.'='.$value;
+            }
+        }
+    
+        return $str;
+    }
+    
 }
 
 header("Content-Type: application/json");
